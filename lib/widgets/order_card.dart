31ea4849b02c979e198432b6e7e19e326c35b9d4
sun tianhai <suntianhai@suns-Mac-Studio.local>
2023-03-04 16:45:29 +0900

    order card collapse

--- lib/widgets/order_card.dart
+++ lib/widgets/order_card.dart
@@ -1,11 +1,20 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_shop_app/models/order_item.dart';
 import 'package:intl/intl.dart';
 
-class OrderCard extends StatelessWidget {
+class OrderCard extends StatefulWidget {
   final OrderItem item;
 
   const OrderCard({Key? key, required this.item}) : super(key: key);
+
+  @override
+  State<OrderCard> createState() => _OrderCardState();
+}
+
+class _OrderCardState extends State<OrderCard> {
+  var isExpanded = false;
 
   @override
   Widget build(BuildContext context) {
@@ -13,13 +22,42 @@ class OrderCard extends StatelessWidget {
       margin: const EdgeInsets.all(6),
       child: Column(children: [
         ListTile(
-          title: Text("\$${item.amount}"),
-          subtitle: Text(DateFormat("dd MM yyyy hh:mm").format(item.dateTime)),
+          title: Text("\$${widget.item.amount}"),
+          subtitle:
+              Text(DateFormat("dd/MM/yyyy hh:mm").format(widget.item.dateTime)),
           trailing: IconButton(
-            icon: const Icon(Icons.expand_more),
-            onPressed: () {},
+            icon: Icon(isExpanded ? Icons.expand_less : Icons.expand_more),
+            onPressed: () {
+              setState(() {
+                isExpanded = !isExpanded;
+              });
+            },
           ),
-        )
+        ),
+        if (isExpanded)
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+            height: min(widget.item.products.length * 20 + 40, 300),
+            child: ListView.builder(
+              itemBuilder: (ctx, index) {
+                final product = widget.item.products[index];
+                return Row(
+                  children: [
+                    Text(
+                      product.title,
+                      style: const TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "${product.quantity}x  \$${product.price}",
+                      style: const TextStyle(fontSize: 18, color: Colors.grey),
+                    )
+                  ],
+                );
+              },
+              itemCount: widget.item.products.length,
+            ),
+          )
       ]),
     );
   }
